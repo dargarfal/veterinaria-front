73e9adee3265c52c83287333e30881fef8ed4f34
Ernesto Darge <ernesto@ernesto-lap.home>
2020-05-27 05:08:02 +0100

    pacientes creado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+//importando axios
+import clienteAxios from './config/axios';
 
 //Componentes
 import Pacientes from './components/Pacientes';
@@ -7,10 +10,28 @@ import NuevaCita from './components/NuevaCita';
 import Cita from './components/Cita';
 
 function App() {
+
+  //State
+const [citas, guardarCitas] = useState([]);
+
+useEffect( () => {
+  const consultarAPI = () => {
+    clienteAxios.get('/pacientes')
+      .then(res => {
+        guardarCitas(res.data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  consultarAPI();
+}, []); 
+
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Pacientes} />
+        <Route exact path="/" component={ () => <Pacientes citas={citas}/>} />
         <Route exact path="/nueva" component={NuevaCita} />
         <Route exact path="/cita/:id" component={Cita} />  
       </Switch>  
